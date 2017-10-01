476939c536f4275dfb917b659413e225cdc44df4
someonesgarden <d@someonesgarden.org>
2017-10-02 01:21:15 +0900

    test2

---
+++ AVRPrograming/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * AVRPrograming.cpp
+ *
+ * Created: 2017/10/02 1:10:29
+ * Author : someonesgarden
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
